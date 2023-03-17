76b8b139fd0f1ab76ccb117f4ac540b106b4a933
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-16 22:06:11 -0300

    Update exercicio01.py

--- semana01/exercicio01.py
+++ semana01/exercicio01.py
@@ -1,1 +1,25 @@
-print("Hello")
+idade = 18
+nome = "Nicolas"
+cpf = "111.222.333-44"
+peso = 60
+dataNascimento = "01/01/1000"
+cidade = "Santa Maria"
+estado = "Rio Grande do Sul"
+velocidade = 80
+preco = 100
+
+print("Variável Inteira Idade: ", idade)
+print("Variável Char Nome: ", nome)
+print("Variável Char Cpf: ", cpf)
+print("Variável Float Peso: ", peso)
+print("Variável Char Data de Data de Nascimento: ", dataNascimento)
+print("Variável Char Cidade: ", cidade)
+print("Variável Char Estado: ", estado)
+print("Variável Float Velocidade: ", velocidade)
+print("Variável Float Preço: ", preco)
+
+nome = "Nicolas"
+idade = 18
+peso = 50
+
+print("Olá, seu nome é", nome, "você tem", idade,"anos e pesa", peso,"kg")
