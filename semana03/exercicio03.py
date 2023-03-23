f4d7a25bd4b3b830db921e6b463e4b8de90346c7
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-23 20:21:47 -0300

    Create exercicio03.py

---
+++ semana03/exercicio03.py
@@ -0,0 +1,10 @@
+#Exemplo:
+
+totalHoras = float(input("Digite o total de horas trabalhadas no mês: "))
+salario = totalHoras * 35
+if salario < 1000.00:
+    n2 = salario + 300.00
+    print("já que seu salário normal: R$", salario, "foi inferior a R$1000, demos um bonûs de R$300")
+    print("seu salário final é de: R$", n2)
+else:
+    print("seu salário esse mês é de: R$", salario)
