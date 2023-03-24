c4837395550851946e399dcbba37c1f1825c88df
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-23 21:51:36 -0300

    Update exercicio03.py

--- semana03/exercicio03.py
+++ semana03/exercicio03.py
@@ -8,3 +8,49 @@ if salario < 1000.00:
     print("seu salário final é de: R$", n2)
 else:
     print("seu salário esse mês é de: R$", salario)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu peso ideal, utilizando as seguintes formulas (onde ´ h corresponde a altura): 
+#Homens: (72.7 ∗ h) − 58
+#Mulheres: (62, 1 ∗ h) − 44, 7
+    
+    h = float(input("Digite sua altura: "))
+generoMasculino = (72.7 * h) - 58
+generoFeminino = (62.1 * h) - 44.7
+genero = input("Digite seu gênero: ")
+if genero == "Masculino":
+    print("Seu peso ideal é de: ", generoMasculino)
+if genero == "Feminino":
+    print("Seu peso ideal é de: ", generoFeminino)
+else:
+    print("valor inválido")
