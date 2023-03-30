f69b4b179734a877520197ee9c78ac5c2019df0a
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-30 20:34:24 -0300

    Update exercicio04.py

--- semana04/exercicio04.py
+++ semana04/exercicio04.py
@@ -15,3 +15,31 @@ elif n2 > n1:
     print("O menor valor é:", n1)
 else:
     print("Os valores são iguais")
+    
+    #Um sistema de uma loja de roupas permite que as peças de roupas sejam vendidas de três formas diferentes: 
+#À vista.
+#2 vezes.
+#3 vezes.
+#Para isso, o sistema deve ler o valor da peça, a opção de pagamento e apresentar o valor das parcelas.
+
+precoRoupa = float(input("Digite o valor do produto: "))
+
+print("1 - À vista")
+print("2 - Parcelar em 2 vezes")
+print("3 - Parcelar em 3 vezes")
+opcao = int(input("Digite a opção desejada: "))
+
+if opcao == 1:
+    print("O valor da roupa à vista é: ", precoRoupa)
+elif opcao == 2:
+    valorParcela = precoRoupa / 2
+    print("O valor da roupa é: ", precoRoupa)
+    print("O valor parcelado em 2 vezes é: ", valorParcela)
+elif opcao == 3:
+    valorParcela = precoRoupa / 3
+    print("O valor da roupa é: ", precoRoupa)
+    print("O valor parcelado em 3 vezes é: ", valorParcela)
+else:
+    print("Digite uma opção válida")
+    
+    
