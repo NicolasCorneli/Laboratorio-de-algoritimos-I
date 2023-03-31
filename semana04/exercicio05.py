f0ef4e01f9c55bf49292a46293fd37ef5613ba0b
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-30 22:17:59 -0300

    Update exercicio05.py

--- semana04/exercicio05.py
+++ semana04/exercicio05.py
@@ -12,3 +12,18 @@ else:
     print(nome, "você recebeu um aumento de 5%, seu salário foi de: ", salarioNovo)
     
     
+#Faça um script que peça os 3 lados de um triângulo. O script deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+
+
+base = float(input("Digite o valor da base do triângulo: "))
+cateto1 = float(input("Digite o valor de um cateto do triângulo: "))
+cateto2 = float(input("Digite o valor do outro cateto do triângulo: "))
+
+if base == cateto1 and cateto1 == cateto2:
+    print("O triângulo é equilátero")
+elif base == cateto1 or base == cateto2 or cateto1 == cateto2:
+    print("O triângulo é isósceles")
+elif base != cateto1 and base != cateto2 and cateto2 != cateto1:
+    print("O triângulo é escaleno")
+    
+    
