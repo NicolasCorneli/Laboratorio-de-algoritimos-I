a3ef29f7545f866586396652a2a19caef22bbfd5
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-03-30 23:55:44 -0300

    Update exercicio05.py

--- semana04/exercicio05.py
+++ semana04/exercicio05.py
@@ -41,3 +41,22 @@ else:
     print("Você está reprovado")
     
     
+#Faça um algoritmo que leia as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+
+
+n1 = float(input("Digite sua nota: "))
+n2 = float(input("Digite sua nota: "))
+media = (n1 + n2) / 2
+
+if media >= 9 and media <= 10:
+    print("Conceito A, suas notas foram ",n1,"e",n2,"sua média foi: ",media,"Você foi aprovado")
+elif media >= 7.5 and media < 9:
+    print("Conceito B, suas notas foram ",n1,"e",n2,"sua média foi: ",media,"Você foi aprovado")
+elif media >= 6 and media < 7.5:
+    print("Conceito C, suas notas foram ",n1,"e",n2,"sua média foi: ",media,"Você foi aprovado")
+elif media >= 4 and media < 6:
+    print("Conceito D, suas notas foram ",n1,"e",n2,"sua média foi: ",media,"Você foi reprovado")
+elif media >= 0 and media < 4:
+    print("Conceito E, suas notas foram ",n1,"e",n2,"sua média foi: ",media,"Você foi reprovado")
+
+#Faça um script que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
