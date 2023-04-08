18a740bf95702f6864d64249e2c566dd46c5f383
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-04-08 19:14:56 -0300

    Update exercicio06.py

--- semana05/exercicio06.py
+++ semana05/exercicio06.py
@@ -27,3 +27,29 @@ while opcao != 3:
         print("A média oficial de alturas é de: ",mediaA)
     else:    
         print("Digite uma opção válida")
+
+
+#Faça um algoritmo que simule uma conta bancária:
+
+saldoAtual = float(input("Digite o seu saldo atual: "))
+opcao = 0
+while opcao != 4:
+    print("1 - Sacar")
+    print("2 - Depositar")
+    print("3 - Verificar Saldo")
+    print("4 - Sair")
+    opcao = int(input("Digite a opção que deseja: "))
+    if opcao == 1:
+        saque = float(input("Digite o valor que deseja retirar: "))
+        saldoNovo = saldoAtual - saque
+        if saldoAtual < saque:
+            print("Você não tem fundos suficientes para retirar esse valor")
+    elif opcao == 2:
+        deposito = float(input("Digite o valor que deseja depositar: "))
+        saldoNovo = saldoNovo + deposito
+    elif opcao == 3:
+        print("O seu saldo atual é de: ",saldoNovo)
+    elif opcao == 4:
+        print("Até a próxima!")
+    else:
+        print("Digite um opção valido")
