9cca5ccff277a8a7e72231f8847ab36081ff2104
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-04-20 20:16:57 -0300

    Update exercicio07.py

--- semana07/exercicio07.py
+++ semana07/exercicio07.py
@@ -3,3 +3,15 @@
 for x in range(1,11):
     print(x)
     
+#Crie um programa que calcule e exiba na tela a média aritmética de um conjunto de números lidos do usuário utilizando o laço de repetição for.
+
+contador = 0
+soma = 0
+primeiro = int(input("Digite o primeiro número: "))
+segundo = int(input("Digite o segundo número: "))
+for x in range(primeiro+1,segundo):
+    soma = soma + x
+    contador = contador + 1
+    media = soma / contador
+print("A média dos valores entre os números digitados é: ",media)
+
