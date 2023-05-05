3dd4c25f103465905aba93f7abea34a6a2d49588
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-04 21:32:49 -0300

    Update exercicio08.py

--- semana08/exercicio08.py
+++ semana08/exercicio08.py
@@ -10,3 +10,14 @@ def main():
     ola()
     tchau()
 main()
+
+#Faça um algoritmo que receba um valor e apresente o dobro e o triplo do valor.
+
+valor = int(input("Digite um valor: "))
+def dobroTriplo(valor):
+    dobro = valor * 2
+    triplo = valor * 3
+    print("O dobro é: ",dobro,",O triplo é: ",triplo)
+def main():
+    dobroTriplo(valor)
+main()
