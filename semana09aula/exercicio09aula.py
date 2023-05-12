9456dccce0b6dc799abfcbc0fa668a311b588c26
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-11 22:08:22 -0300

    Update exercicio09aula.py

--- semana09aula/exercicio09aula.py
+++ semana09aula/exercicio09aula.py
@@ -20,3 +20,37 @@ main()
 
 #Faça um programa que receba a idade e o peso de 7 pessoas, calcule e mostre:
 
+def media(mediaIdade,mediaPeso,idade,peso):
+    for x in range (1,8):
+        idade = int(input("Digite sua idade: "))
+        peso = float(input("Digite seu peso: "))
+        mediaIdade = mediaIdade + idade
+        mediaPeso = mediaPeso + peso
+    mediaIdade = mediaIdade / 7
+    mediaPeso = mediaPeso / 7
+    print("media idade:",mediaIdade)
+    print("media peso: ",mediaPeso)
+    
+def maiorIdade(idade,qntdMaior):
+    if idade >= 18:
+        qntdMaior = qntdMaior + 1
+    print("quantidade de maiores de idade:",qntdMaior)
+    
+def porcentagem(idade,porcentagemIdades):
+    if idade > 10 and idade < 30:
+        porcentagemIdades = porcentagemIdades + 1
+    porcentagemIdades = porcentagemIdades / 7
+    porcentagemIdades = porcentagemIdades * 100
+    print("porcentagem das idades entre 10 e 30:",porcentagemIdades)
+    
+def main():
+    peso = 0
+    idade = 0
+    mediaPeso = 0
+    mediaIdade = 0
+    qntdMaior = 0
+    porcentagemIdades = 0
+    idade = media(mediaIdade,mediaPeso,idade,peso)
+    maiorIdade(idade,qntdMaior)
+    porcentagem(idade,porcentagemIdades)
+main()
