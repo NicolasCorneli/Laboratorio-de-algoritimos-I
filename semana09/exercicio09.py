a335551be8f3b6941912508b283a295eaeeafa7d
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-16 15:21:22 -0300

    Update exercicio09.py

--- semana09/exercicio09.py
+++ semana09/exercicio09.py
@@ -1,1 +1,12 @@
+#Escreva uma função que receba dois números como parâmetros e retorne a soma deles.
 
+def fsoma(x,y):
+    soma = x + y
+    print(soma)
+
+def main():
+    soma = 0
+    x = int(input("Digite um número: "))
+    y = int(input("Digite um número: "))
+    fsoma(x,y)
+main()
