7f84110f684b1a90e544cbf25d3ca104520c8608
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-16 15:31:14 -0300

    Update exercicio09.py

--- semana09/exercicio09.py
+++ semana09/exercicio09.py
@@ -10,3 +10,21 @@ def main():
     y = int(input("Digite um número: "))
     fsoma(x,y)
 main()
+
+#Escreva uma função que verifique se um número é primo. A função deve receber um número como parâmetro e retornar True se ele for primo, caso contrário, retorna False.
+
+def fprimo(x):
+    if x == 2:
+        print("é primo")
+    for i in range(2,x):
+        if x %i == 0:
+            print("Não é primo")
+            break
+        else:
+            print("é primo")
+            break
+
+def main():
+    x = int(input("Digite um número: "))
+    fprimo(x)
+main()
