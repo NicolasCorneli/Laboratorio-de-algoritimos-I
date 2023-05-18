64ce7cb4c2693010ecb27f1115b0d21976928639
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-18 20:21:26 -0300

    Update exercicio11.py

--- semana11/exercicio11.py
+++ semana11/exercicio11.py
@@ -2,13 +2,15 @@ def y(valores):
     for x in range (1,11):
         valor = int(input("Digite um valor: "))
         valores.append(valor)
-def z(valores):
+def z(valores,qntdMaiores):
     for x in valores:
         if x > 100:
-            print(x)
+            qntdMaiores = qntdMaiores + 1
+    print(qntdMaiores)
 def main():
+    qntdMaiores = 0
     valores = []
     y(valores)
-    z(valores)
+    z(valores,qntdMaiores)
     
 main()
