a228b928e6f1e93c40299114355dc49a113f9212
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-05-25 21:43:08 -0300

    Update exercicio12.py

--- semana12/exercicio12.py
+++ semana12/exercicio12.py
@@ -1,20 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Escreva um programa que cria um array com os números de 1 a 10 e os imprime na ordem inversa.
 
 
 
@@ -49,3 +33,37 @@ def main():
     u(lista,media,soma)
     
 main()   
+
+
+
+
+
+
+
+#Escreva um programa que cria um array com 5 números inteiros digitados pelo usuário. Em seguida, encontre e exiba o maior e o menor número do array e suas respectivas posições. 
+
+
+def y(lista,maior,menor):
+    for x in range(1,5):
+        n = int(input("Digite um número: "))
+        menor = n
+        maior = n
+        lista.append(n)
+def u(lista,maior,menor):
+    for x in lista:
+        if maior < x:
+            maior = x
+        if menor > x:
+            menor = x
+            
+    print("O maior valor é",maior,"A posição do valor na lista é ",lista.index(maior))
+    print("O menor valor é",menor,"A posição do valor na lista é ",lista.index(menor))
+def main():
+    lista = []
+    n = int(input("Digite um número: "))
+    lista.append(n)
+    maior = n
+    menor = n
+    y(lista,maior,menor)
+    u(lista,maior,menor)
+main()
